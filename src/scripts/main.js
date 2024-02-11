2742daf16ce9cc3f1a668133513bb33ae6308465
Volodymyr Kuzmin <vlkzmn@gmail.com>
2024-02-11 21:25:43 +0200

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -20,12 +20,20 @@
 //   return Math.floor(Math.random() * (max - min + 1)) + min;
 // }
 
+window.addEventListener("hashchange", () => {
+  if (window.location.hash === "#menu") {
+    document.body.classList.add("body--with-menu");
+  } else {
+    document.body.classList.remove("body--with-menu");
+  }
+});
+
 const arrowUp = document.querySelector(".footer__up-arrow");
 
 window.addEventListener("scroll", function () {
   if (
-    document.body.scrollTop > 500
-    || document.documentElement.scrollTop > 500
+    document.body.scrollTop > 500 ||
+    document.documentElement.scrollTop > 500
   ) {
     arrowUp.classList.add("footer__up-arrow--show");
   } else {
