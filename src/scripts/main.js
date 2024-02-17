7c0b519bbb2c5c4b5e9f098237c81c9d140446a5
Volodymyr Kuzmin <vlkzmn@gmail.com>
2024-02-17 16:26:40 +0200

    final

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,25 +1,22 @@
-/* eslint-disable operator-linebreak */
-/* eslint-disable space-before-function-paren */
-/* eslint-disable quotes */
-"use strict";
+'use strict';
 
-window.addEventListener("hashchange", () => {
-  if (window.location.hash === "#menu") {
-    document.body.classList.add("body--with-menu");
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('body--with-menu');
   } else {
-    document.body.classList.remove("body--with-menu");
+    document.body.classList.remove('body--with-menu');
   }
 });
 
-const arrowUp = document.querySelector(".footer__up-arrow");
+const arrowUp = document.querySelector('.footer__up-arrow');
 
-window.addEventListener("scroll", function () {
+window.addEventListener('scroll', function() {
   if (
-    document.body.scrollTop > 500 ||
-    document.documentElement.scrollTop > 500
+    document.body.scrollTop > 500
+    || document.documentElement.scrollTop > 500
   ) {
-    arrowUp.classList.add("footer__up-arrow--show");
+    arrowUp.classList.add('footer__up-arrow--show');
   } else {
-    arrowUp.classList.remove("footer__up-arrow--show");
+    arrowUp.classList.remove('footer__up-arrow--show');
   }
 });
